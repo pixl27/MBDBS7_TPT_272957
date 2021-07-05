10070c4deaae83259db468b48cb29b510d2279b1
Mahery <maheryrakotondrazaka@gmail.com>
2021-07-05 10:24:38 +0300

    add toast to sign up

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../shared/auth.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-sign-up',
@@ -12,18 +13,31 @@ export class SignUpComponent implements OnInit {
   prenom!: ''
   username!: ''
   password!: ''
-  constructor(private authservice:AuthService,private router:Router) { }
+  constructor(private authservice:AuthService,private router:Router,private toastr: ToastrService) { }
 
   ngOnInit(): void {
+  }
+  showtoast(title:string,body:string,type:string) {
+    if(type=="success"){
+        this.toastr.success(body, title);
+
+    }
+    else {
+        this.toastr.error(body, title);
+
+    }
   }
 signup(){
   console.log(
     this.authservice.signUp(this.nom,this.prenom,this.username,this.password).subscribe( 
       data => {
-        this.router.navigate(["/"]);
+        this.router.navigate(["/"]).then(() => {
+          this.showtoast("Inscription reussie","félicitation votre inscription à réussie","success");
+          window.location.reload();
+        });;;
 
       },
-      err => console.log(err)
+      err => this.showtoast("Inscription echouée","pseudo déja pris","error")
 
 
     )

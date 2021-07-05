77d9a5bf505b9d168d39ce211b5f8bf6b96ba1b5
Mahery <maheryrakotondrazaka@gmail.com>
2021-07-05 09:52:42 +0300

    add toast to login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { AuthService } from '../shared/auth.service';
 
 @Component({
@@ -10,21 +11,32 @@ import { AuthService } from '../shared/auth.service';
 export class LoginComponent implements OnInit {
   username!: ''
   password!: ''
-  constructor(private authservice:AuthService,private router:Router) { }
+  constructor(private authservice:AuthService,private router:Router,private toastr: ToastrService) { }
 
   ngOnInit(): void {
     this.router.routeReuseStrategy.shouldReuseRoute = () => false;
 
   }
+  showtoast(title:string,body:string,type:string) {
+    if(type=="success"){
+        this.toastr.success(body, title);
 
+    }
+    else {
+        this.toastr.error(body, title);
+
+    }
+  }
   login(){
     console.log(
       this.authservice.logIn(this.username,this.password).subscribe( 
         data => {
-          this.router.navigate(["/"]);
+          this.router.navigate(["/"]).then(() => {
+            window.location.reload();
+          });;
   
         },
-        err => console.log("erreur de  connexion")
+        err => {this.showtoast("Erreur de Connexion","veuiller verifiez votre pseudo ou mot de passe","error")}
   
   
       )
